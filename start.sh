e10c772a29299eefbf842e1413f80196e117f430
alexnguyen1606 <48585657+alexnguyen1606@users.noreply.github.com>
2024-03-28 20:19:35 +0700

    update start

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python ~/ImageAlchemy/neurons/miners/StableMiner/main.py --wallet.name nxanh --wallet.hotkey h2 --netuid 25 --subtensor.network test --axon.port 1229 --logging.debug --miner.device cuda:0 --wandb.api_key 3d50995bc01ec930a7cb58a07bc611551eb598f4 --wandb.entity ImageAlchemy --wandb.project ImageAlchemy
+python ~/ImageAlchemy/neurons/miners/StableMiner/main.py --wallet.name nxanh --wallet.hotkey h2 --netuid 25 --subtensor.network test --axon.port 1229 --logging.debug --miner.device cuda:0 --wandb.api_key 3d50995bc01ec930a7cb58a07bc611551eb598f4 --wandb.entity natuan1606 --wandb.project ImageAlchemy
 --miner.model natuan-1606/test
